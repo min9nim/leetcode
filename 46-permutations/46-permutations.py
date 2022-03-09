7816f0c54a36ee7391fabab4903ab9ef45df9b40
Keating <min9nim@gmail.com>
2022-03-10 05:58:24 +0900

    Time: 57 ms (52.14%), Space: 14 MB (74.15%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,14 @@
+class Solution:
+  def permute(self, nums):
+    result = [[nums.pop()]]
+
+    for num in nums :
+      new_result = []
+      for i in range(len(result[0])+1) : 
+        for arr in result :
+          copy = arr[:]
+          copy.insert(i,num)
+          new_result.append(copy)
+      result = new_result
+
+    return result
