9e57e8b93acea4122eeaa24bd6afeb65f09b2797
Keating <min9nim@gmail.com>
2022-03-10 05:55:49 +0900

    Time: 3546 ms (8.39%), Space: 77.2 MB (7.05%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,18 @@
+var threeSum = function(nums) {
+    const set = new Set()
+    
+    for(let i=0; i<nums.length-2; i++){
+        const map = new Map()
+        for(let j=i+1; j<nums.length; j++){
+            if(map.has(nums[j])){
+                const [a,b] = map.get(nums[j])
+                const arr = [nums[a], nums[b], nums[j]]
+                arr.sort((a,b) => (a-b))
+                set.add(arr.join(','))
+            }else{
+                map.set(-nums[i] - nums[j], [i,j])
+            }
+        }    
+    }
+    return Array.from(set).map(str => str.split(',').map(Number))
+};
