a8f61646e4cd8e9afb56d370c2abbdcb8d7d750f
Keating <min9nim@gmail.com>
2022-03-10 05:49:43 +0900

    Time: 150 ms (58.83%), Space: 48.1 MB (14.42%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let flag = 0
+    const head = new ListNode()
+    let current = head
+    
+    while(l1 || l2 || flag){
+        const sum = (l1?.val || 0) + (l2?.val || 0) + flag
+        current.val = sum%10
+        flag = sum > 9 ? 1 : 0
+       
+        if(!l1?.next && !l2?.next && !flag){
+            break   
+        }
+        
+        if(!l1 && !flag){
+            current.next = l2.next
+            break
+        }
+        if(!l2 && !flag){
+            current.next = l1.next
+            break
+        }
+        
+ 
+        current.next = new ListNode()
+        current = current.next
+        
+        if(l1){
+            l1 = l1.next
+        }
+        if(l2){
+            l2 = l2.next
+        }
+    }
+    
+    return head
+    
+};
+
+ function ListNode(val, next) {
+     this.val = (val===undefined ? 0 : val)
+     this.next = (next===undefined ? null : next)
+ }
+
