868f55def89c51248fb8b360ea062b243d9a47fa
Keating <min9nim@gmail.com>
2022-03-10 06:05:45 +0900

    Time: 48 ms (47.80%), Space: 13.9 MB (41.03%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n < 1:
+            return False
+        while True:
+            if n % 2 == 0:
+                n = n / 2
+            elif n % 3 == 0:
+                n = n / 3
+            elif n % 5 == 0:
+                n = n / 5
+            else:
+                return n == 1
+
+        
