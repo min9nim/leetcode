278f0b4c7d4b0e35c24330a4689814359c415fad
Keating <min9nim@gmail.com>
2023-10-07 00:45:07 +0900

    Time: 70 ms (19.7%), Space: 44.3 MB (71.53%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.js
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.js
@@ -14,9 +14,6 @@ var sortedArrayToBST = function(nums) {
     if(nums.length === 0){
         return null
     }
-    if(nums.length === 1){
-        return new TreeNode(nums[0])
-    }
     const pivotIdx = Math.floor(nums.length/2)
     return new TreeNode(
         nums[pivotIdx],
